b04891c14ed07b588bde93ff58354cfc703143a9
Rezyyn <73084106+Rezyyn@users.noreply.github.com>
2025-01-05 20:32:41 +0000

    Update __init__.py

--- discord_cog/__init__.py
+++ discord_cog/__init__.py
@@ -1,10 +1,5 @@
-import json
-from pathlib import Path
-from .april import AprilCog
+from .april_cog import AprilCog
 
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
-
-async def setup(bot):
+def setup(bot):
+    """Load the AprilCog cog."""
     bot.add_cog(AprilCog(bot))
